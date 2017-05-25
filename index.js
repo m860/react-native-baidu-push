4532f328df244cb706792fdf7f5a1eca8cf71e8a
victor.w.ye <victor.w.ye@yzw.cn>
2017-05-25 18:45:06 +0800

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { NativeModules } from 'react-native';
 
-const { RNBaiduPush } = NativeModules.BDMapManager;
+const { RNBaiduPush } = NativeModules.BDPushManager;
 
 export default RNBaiduPush;
