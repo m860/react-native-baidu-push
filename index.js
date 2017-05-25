7584c0487a935edd78fba35a17044ff935612cea
victor.w.ye <victor.w.ye@yzw.cn>
2017-05-25 18:38:05 +0800

    add

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-import React, { PropTypes } from 'react';
-import {
-    View,
-    requireNativeComponent,
-    NativeModules
+import { NativeModules } from 'react-native';
 
-} from 'react-native';
+const { RNBaiduPush } = NativeModules.BDMapManager;
 
-var pushManager = NativeModules.BDPushManager;
-
-
-const BDPushManager = React.createClass({
-    render: function() {
-        return null;
-    },
-    //RN调用原生方法：
-    startPushWork(apiKey)
-    {
-        pushManager.startPushWork(apiKey);
-    },
-    stopPushWork()
-    {
-        pushManager.stopPushWork();
-    },
-    resumePushWork()
-    {
-        pushManager.resumePushWork();
-    },
-    setPushTags(tags)
-    {
-        pushManager.setPushTags(tags);
-    },
-    removePushTags(tags){
-        pushManager.removePushTags(tags);
-    }
-
-});
-
-
-module.exports = BDPushManager;
+export default RNBaiduPush;
